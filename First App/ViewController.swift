45023735989e6a37a8d77cac5436724eb6d1a30e
Horea Crisan <horea.crisan@softvision.ro>
2020-01-07 16:34:43 +0200

    initial commit

---
+++ First App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by horea.crisan on 07/01/2020.
+//  Copyright © 2020 horea.crisan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
